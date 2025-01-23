b083cfa6ba632ce113440a2f21d7ea35ab02997e
Rutujarajput25 <147191052+Rutujarajput25@users.noreply.github.com>
2025-01-23 19:57:45 +0530

    Add files via upload

---
+++ MY_JAVA/Name.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Name 
+{
+   public static void main (String args[]){
+     Scanner s = new Scanner(System.in);
+     String name;
+     System.out.println("MAY I KNOW UR NAME PLZ");
+     name = s.nextLine();
+     System.out.println("WELCOME!!!");
+ }
+}
